16970219fd51a92eb4d35f7c54c2a37b556ec20c
Leonardo Campos <leonardoworld@outlook.com>
2023-01-02 17:16:25 -0300

    02012023

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 function onScroll() {
   if (scrollY > 45) {
-    barNav.classList.add('scroll')
+    barNavigation.classList.add('scroll')
   } else {
-    barNav.classList.remove('scroll')
+    barNavigation.classList.remove('scroll')
   }
 }
 
@@ -12,3 +12,19 @@ function openMenu() {
 function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
+
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 1000,
+}).reveal(`
+#home,
+#home .imgHeader,
+#home .stats,
+#services,
+#services header,
+#services .card,
+#about, 
+#about header,
+#about .content`)
